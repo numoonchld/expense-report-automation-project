5885844d634d74c5d22d7c01f4cfaedaee1f8296
numoonchld <raghavendra.saralaya@gmail.com>
2021-03-03 08:26:09 +0530

    feb csv generated

--- process_handler_00_rename_files.py
+++ process_handler_00_rename_files.py
@@ -11,7 +11,7 @@ import re
 
 current_dir = os.getcwd()
 
-image_dir = 'images-jan-2021'
+image_dir = 'images-feb-2021'
 image_dir_path = os.path.join(current_dir,image_dir)
 
 image_names = os.listdir(image_dir_path)
